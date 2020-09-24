bd512ff2f62224abcfba78667a12e633198b753c
Ishihara Felipe <ishihara@mail.dcsnet.co.jp>
2020-09-24 09:53:21 +0900

    Add POST method to app.js

--- app.js
+++ app.js
@@ -8,5 +8,10 @@ app.use(exp.json())
 const stat = require("serve-static")
 app.use(stat("public", {"index": "main.html"}))
 
+app.post("/data", async (req, res) => {
+	const json = require("./data/people.json")
+	res.send(json)
+})
+
 const port = process.env.PORT || 8000
 app.listen(port, () => console.log(`Connected to port ${port}!`))
